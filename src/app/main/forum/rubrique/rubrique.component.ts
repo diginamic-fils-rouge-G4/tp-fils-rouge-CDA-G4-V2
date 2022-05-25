f14b7d59bdcf38ce487696feefc8006325bc78b2
AlshamiWasin <waseem.fr1999@gmail.com>
2022-05-25 17:11:20 +0200

    rubrique button edit effect

--- src/app/main/forum/rubrique/rubrique.component.ts
+++ src/app/main/forum/rubrique/rubrique.component.ts
@@ -8,36 +8,70 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class RubriqueComponent implements OnInit {
 
-  addclass: boolean = false
+  selectedId:any;
+
+  rubriques =[
+    {id:1,nom:"test1",topicnumber:15},
+    {id:2,nom:"test2",topicnumber:15},
+    {id:3,nom:"test3",topicnumber:15},
+    {id:4,nom:"test4",topicnumber:15},
+    {id:5,nom:"test5",topicnumber:15},
+  ]
+
+  addClass: boolean = false
 
   form = new FormGroup({
-    name: new FormControl(Validators.minLength(2))
+    name: new FormControl('',Validators.minLength(2))
   });
 
   constructor(private elementRef: ElementRef) { }
 
-  ngOnInit(): void {
-    const icon: any = document.querySelector('.ellipse')
-    const navRubrique: any = document.querySelector('.nav-rubrique')
-    icon.addEventListener('click',() => {
-      navRubrique.classList.toggle('d-none')
-      icon.classList.toggle('fa-ellipsis-vertical')
-      icon.classList.toggle('fa-xmark')
-    })}
+  ngOnInit(): void {   
+    };
+
+
 
   ngAfterViewInit() {
     this.elementRef.nativeElement.querySelector('.forumHeaderIcon').addEventListener('click', this.onClick.bind(this));
     this.elementRef.nativeElement.querySelector('.valider-btn').addEventListener('click', this.onClick.bind(this));
+    this.elementRef.nativeElement.querySelector('.annuler-btn').addEventListener('click', this.onClick.bind(this));
     this.elementRef.nativeElement.querySelector('.modal-overlay').addEventListener('click', this.onClick.bind(this));
+    this.elementRef.nativeElement.querySelector('.modal-container').addEventListener('click', this.onClick.bind(this));
+    
+  }
+
+  openVerticale(id:number){
+    if (!this.selectedId) {
+      this.selectedId = id
+    }
+    this.closeVerticale(this.selectedId)
+    this.selectedId = id
+    const navRubrique: any = document.querySelector("#rub"+id);
+    const icon: any = document.querySelector("#icon"+id);
+    const xicon: any = document.querySelector("#Xicon"+id);
+    navRubrique.classList.remove('d-none')
+    icon.classList.add('fa-ellipsis-vertical')
+    icon.classList.add('d-none')
+    xicon.classList.remove('d-none')
+    
+  }
+
+  closeVerticale(id:number){
+    const navRubrique: any = document.querySelector("#rub"+id);
+    const icon: any = document.querySelector("#icon"+id);
+    const xicon: any = document.querySelector("#Xicon"+id);
+    navRubrique.classList.add('d-none')
+    icon.classList.add('fa-ellipsis-vertical')
+    icon.classList.remove('d-none')
+    xicon.classList.add('d-none')
   }
 
   onClick() {
-    if (this.addclass == false) {
-      this.addclass = true
+    
+    if (this.selectedId) {
+      
     }
-    else{
-      this.addclass = false
-    }
+    this.addClass = !this.addClass;
   }
 
 
