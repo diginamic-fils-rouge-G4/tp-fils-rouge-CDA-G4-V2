36f73ef784f34d5025e7c40c6ca74c86f0519664
tgentreau <gentreau.thomas@gmail.com>
2022-05-25 14:30:00 +0200

    fix formulaire login/signup

--- src/app/_shared/components/header/header.component.ts
+++ src/app/_shared/components/header/header.component.ts
@@ -8,7 +8,8 @@ import { FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms'
 })
 export class HeaderComponent implements OnInit {
 
-  formSubmitted: boolean = false
+  formSubmittedLogin: boolean = false
+  formSubmittedSignup: boolean = false
   formLogin = new FormGroup({
     mail: new FormControl('',
       [
@@ -24,15 +25,52 @@ export class HeaderComponent implements OnInit {
     )
   });
 
-  formsignup = {
-    mail: '',
-    password: '',
-    adresse: '',
-    cp: '',
-    ville: '',
-    name: '',
-    firstname: ''
-  }
+  formsignup = new FormGroup({
+    name: new FormControl('', 
+      [
+        Validators.required,
+        Validators.minLength(2)
+      ]
+    ),
+    firstname: new FormControl('', 
+      [
+        Validators.required,
+        Validators.minLength(2)
+      ]
+    ),
+    mail: new FormControl('',
+      [
+        Validators.required, 
+        Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")
+      ]
+    ),
+    city: new FormControl('', 
+      [
+        Validators.required,
+        Validators.minLength(3)
+      ]
+    ),
+    cp: new FormControl('', 
+      [
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(5),
+        Validators.pattern("^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{12,}$")
+      ]
+    ),
+    password: new FormControl('',
+      [
+        Validators.required,
+        Validators.minLength(12)
+      ]
+    ),
+    confirmPassword: new FormControl('',
+      [
+        Validators.required,
+        Validators.minLength(12)
+      ]
+    )
+  })
 
   constructor() { }
 
@@ -40,12 +78,20 @@ export class HeaderComponent implements OnInit {
   }
 
   onSubmitLogin(): void {
-    this.formSubmitted = true
-    console.log(this.formLogin.controls['password'])
+    this.formSubmittedLogin = true
+    console.log(this.formLogin)
   }
 
   onSubmitSignup(): void {
+    this.formSubmittedSignup = true
+    console.log(this.formsignup)
+    this.passwordConfirmValid()
 
+  }
+
+  passwordConfirmValid(): any {
+    return this.formsignup.controls['password'].value === this.formsignup.controls['confirmPassword'].value
+      ? null : {'mismatch': true};
   }
 
 }
