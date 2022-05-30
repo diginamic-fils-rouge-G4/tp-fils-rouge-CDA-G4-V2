3a74cd3e33384009fe1129d7c680bc02d4f65522
AlshamiWasin <waseem.fr1999@gmail.com>
2022-05-30 11:45:33 +0200

    login , signup cleck effect fait

--- src/app/_shared/components/header/header.component.ts
+++ src/app/_shared/components/header/header.component.ts
@@ -1,12 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+import { AfterViewInit, Component, ElementRef, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit ,AfterViewInit{
+
+  loginVisible:boolean =false;
+  SignupVisible:boolean =false;
 
   formSubmittedLogin: boolean = false
   formSubmittedSignup: boolean = false
@@ -72,26 +75,72 @@ export class HeaderComponent implements OnInit {
     )
   })
 
-  constructor() { }
+  constructor(private elementRef: ElementRef) { }
 
   ngOnInit(): void {
   }
 
+  ngAfterViewInit(){
+
+    this.elementRef.nativeElement.querySelector('.Connexion').addEventListener('click', this.onClickLogin.bind(this));
+
+    this.elementRef.nativeElement.querySelector('.Inscription').addEventListener('click', this.onClickSignup.bind(this));
+
+    this.elementRef.nativeElement.querySelector('#closeCon').addEventListener('click', this.onClickLogin.bind(this),);
+
+    this.elementRef.nativeElement.querySelector('#closeIns').addEventListener('click', this.onClickSignup.bind(this));
+
+    // reset les forms avec le X
+    // this.elementRef.nativeElement.querySelector('#closeCon').addEventListener('click', this.resetForm.bind(this),);
+
+    // this.elementRef.nativeElement.querySelector('#closeIns').addEventListener('click', this.resetForm.bind(this));
+    //
+
+    this.elementRef.nativeElement.querySelector('#containerCon').addEventListener('click', this.onClickLogin.bind(this));
+
+    this.elementRef.nativeElement.querySelector('#wrapperCon').addEventListener('click', this.onClickLogin.bind(this));
+
+    this.elementRef.nativeElement.querySelector('#containerIns').addEventListener('click', this.onClickSignup.bind(this));
+
+    this.elementRef.nativeElement.querySelector('#wrapperIns').addEventListener('click', this.onClickSignup.bind(this));
+
+  }
+
   onSubmitLogin(): void {
     this.formSubmittedLogin = true
+    this.formSubmittedSignup = false
     console.log(this.formLogin)
   }
 
   onSubmitSignup(): void {
     this.formSubmittedSignup = true
+    this.formSubmittedLogin = false
     console.log(this.formsignup)
     this.passwordConfirmValid()
 
   }
 
+  // function pour reset les valeurs et errors des forms
+  // resetForm(){
+  //   this.formsignup.reset()
+  //   this.formLogin.reset()
+  //   this.formSubmittedLogin = false
+  //   this.formSubmittedSignup = false
+  // }
+
+
   passwordConfirmValid(): any {
     return this.formsignup.controls['password'].value === this.formsignup.controls['confirmPassword'].value
       ? null : {'mismatch': true};
+  }
+
+
+  onClickLogin() {
+    this.loginVisible = !this.loginVisible;
+  }
+
+  onClickSignup() {
+    this.SignupVisible = !this.SignupVisible;
   }
 
 }
