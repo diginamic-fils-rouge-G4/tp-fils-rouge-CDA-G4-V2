33cc3057d8ea47253e61a6a531320f2892c926b5
tgentreau <gentreau.thomas@gmail.com>
2022-08-18 15:11:21 +0200

    post service/controlleur

--- src/main/java/dev/repository/PostRepository.java
+++ src/main/java/dev/repository/PostRepository.java
@@ -4,6 +4,8 @@ import dev.entite.forum.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PostRepository extends JpaRepository<Post, Integer> {
 }
