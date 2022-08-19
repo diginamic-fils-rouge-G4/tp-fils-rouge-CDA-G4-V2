368b7404b3da2e576243da903cebdad1bf2eae6a
Mysteriou85 <guib5@hotmail.fr>
2022-08-19 16:51:57 +0200

    Completion du DTO PostExportDTO

--- src/main/java/dev/controller/dto/post/PostExportDTO.java
+++ src/main/java/dev/controller/dto/post/PostExportDTO.java
@@ -1,4 +1,52 @@
 package dev.controller.dto.post;
 
+import java.time.LocalDateTime;
+
+// DTO utilisé pour les informations affichées au FRONT
 public class PostExportDTO {
+
+    // Nom de l'utilisateur qui effectué le post
+    String utilisateur;
+
+    // Contenue du post
+    String content;
+
+    // Date du post
+    LocalDateTime date;
+
+    // Heure du post
+    LocalDateTime heure;
+
+    // Getter & Setter
+    public String getUtilisateur() {
+        return utilisateur;
+    }
+
+    public void setUtilisateur(String utilisateur) {
+        this.utilisateur = utilisateur;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public LocalDateTime getHeure() {
+        return heure;
+    }
+
+    public void setHeure(LocalDateTime heure) {
+        this.heure = heure;
+    }
 }
