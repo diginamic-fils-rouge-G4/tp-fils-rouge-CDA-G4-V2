19bc98a5ba07d2e98618ed42df05d26ffc414f5a
Mysteriou85 <guib5@hotmail.fr>
2022-08-19 16:47:30 +0200

    Ajout du DTO RubriqueAdminExportDTO & RubriqueExportDTO

--- src/main/java/dev/controller/dto/rubrique/RubriqueExportDTO.java
+++ src/main/java/dev/controller/dto/rubrique/RubriqueExportDTO.java
@@ -1,4 +1,28 @@
 package dev.controller.dto.rubrique;
 
+// DTO utilisé pour les informations affichées au FRONT
 public class RubriqueExportDTO {
+
+    // Libelle de la rubrique
+    private String libelle;
+
+    // Nombre de topic présent dans la rubrique
+    private Integer nbreTopics;
+
+    // Getter & Setter
+    public String getLibelle() {
+        return libelle;
+    }
+
+    public void setLibelle(String libelle) {
+        this.libelle = libelle;
+    }
+
+    public Integer getNbreTopics() {
+        return nbreTopics;
+    }
+
+    public void setNbreTopics(Integer nbreTopics) {
+        this.nbreTopics = nbreTopics;
+    }
 }
