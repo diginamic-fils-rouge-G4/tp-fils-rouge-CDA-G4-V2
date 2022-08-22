ef9983791e6b92cfa488121fb23741bb95171b26
AlshamiWasin <waseem.fr1999@gmail.com>
2022-08-22 09:38:38 +0200

    station + ville dto

--- src/main/java/dev/controller/dto/ville/VilleDTO.java
+++ src/main/java/dev/controller/dto/ville/VilleDTO.java
@@ -1,4 +1,26 @@
 package dev.controller.dto.ville;
 
+import dev.controller.dto.station.StationDTO;
+
+import java.util.List;
+
 public class VilleDTO {
+    private String name;
+    private List<StationDTO> stations;
+    public VilleDTO() {
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("VilleDTO{");
+        sb.append("name='").append(name).append('\'');
+        sb.append(", stations=").append(stations);
+        sb.append('}');
+        return sb.toString();
+    }
 }
