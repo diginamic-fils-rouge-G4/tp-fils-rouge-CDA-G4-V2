9379648855e57c9364d0f7f6d8f5e2bed9d9563b
thibaultMA <thibault.m-a@outlook.com>
2023-04-06 12:11:33 +0200

    fin regex

--- src/app/_shared/components/header/header.component.ts
+++ src/app/_shared/components/header/header.component.ts
@@ -34,7 +34,7 @@ export class HeaderComponent implements OnInit, AfterViewInit {
   formLogin = new FormGroup({
     mail: new FormControl('', [
       Validators.required,
-      Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'),
+      Validators.pattern('^[a-zA-Z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'),
     ]),
     password: new FormControl('', [
       Validators.required,
@@ -52,7 +52,7 @@ export class HeaderComponent implements OnInit, AfterViewInit {
     ]),
     mail: new FormControl('', [
       Validators.required,
-      Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'),
+      Validators.pattern('^[a-zA-Z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'),
     ]),
     ville: new FormControl('', [Validators.required, Validators.minLength(3)]),
     cp: new FormControl('', [
